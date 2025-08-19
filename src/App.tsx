48f0f9941a0881ff9d89b3e669bcf613cb42368e
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-08-19 14:38:46 -0300

    Ajustando problemas de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import './App.css'
 
@@ -6,16 +6,17 @@ import MenuPrincipal from "./features/administrador/pages/menuPrincipal/menuPrin
 import Login from "./features/administrador/pages/login/login";
 
 function App() {
-  
   return (
     <Router>
-        <ToastContainer position="top-right" autoClose={3000} />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<MenuPrincipal />} />
-        </Routes>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<MenuPrincipal />} />
+        <Route path="/index.html" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
 export default App
