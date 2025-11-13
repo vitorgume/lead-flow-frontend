f6500d1d4fe315164b49e8168abb3be3b970d02b
vitorgume <vitorhvvieira@gmail.com>
2025-11-13 09:44:54 -0300

    Ajustando URL do backend

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://iw3ggxiaik.us-east-1.awsapprunner.com/',
 });
 
 api.interceptors.request.use((config) => {
